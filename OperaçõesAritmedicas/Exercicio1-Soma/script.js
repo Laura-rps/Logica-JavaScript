97dde902ab0ef58b0ed8db5cb06622f0e90d114a
Laura Roberto <lauraroberto731@gmail.com>
2025-04-04 23:50:53 -0300

    JavaScript

---
+++ OperaçõesAritmedicas/Exercicio1-Soma/script.js
@@ -0,0 +1,5 @@
+alert("Soma de dois numeros inteiros.")
+let numero1 = parseInt(prompt("Digite o primeiro numero: "))
+let numero2 = parseInt(prompt("Digite o segundo numero: "))
+let resultado = numero1 + numero2
+alert("A soma dos numeros " + numero1 + " e " + numero2 + " é igual a: " + resultado + ".")
---
+++ OperaçõesAritmedicas/Exercicio4-Divisão/script.js
@@ -0,0 +1,5 @@
+alert("Divisão de dois numeros inteiros.")
+let numero1 = parseFloat(prompt("Digite o primeiro numero: "))
+let numero2 = parseFloat(prompt("Digite o segundo numero: "))
+let resultado = numero1 / numero2
+alert("O resultado da divisão dos numeros " + numero1 + " e " + numero2 + " é igual a: " + resultado)  
